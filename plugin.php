582ce0f18cdb735e4a5a9a3faab68bc803e80eb9
Illia Kovalov <ikovalyov85@gmail.com>
2018-02-19 00:50:15 +0100

    bugfixes and style

--- plugin.php
+++ plugin.php
@@ -1,63 +1,90 @@
 <?php
 /*
- * Plugin Name: bbPress Unread Posts v2
+ * Plugin Name: bbPress Unread Posts v3
  * Description: Displays an icon next to each thread and forum if there are unread posts for the current user in it.
- * Version: 1.0.8
- * Author: coronoro
+ * Based on https://github.com/coronoro/bbPress-Unread-Posts-v2
+ * Version: 1.0.0
+ * Author: ikovalyov
  * Text Domain: bbpress-unread-posts-v2
  * Domain Path: /languages
  */
 include 'logging.php';
 include 'bbp_unread_options.php';
-//include 'bbp_unread_shortcode.php';
 
-class unreadInformation{};
+/**
+ * Class UnreadInformation
+ */
+class UnreadInformation
+{
+	/**
+	 * Is Unread.
+	 *
+	 * @var bool
+	 */
+	public $unread;
 
-add_action ( "init", "bbp_unread_posts_Initialize" );
-add_action ( 'admin_menu', 'bbp_unread_posts_plugin_menu' );
-// add_shortcode( 'unread_posts', 'bbp_show_unread_posts' );
+	/**
+	 * Total amount.
+	 *
+	 * @var int
+	 */
+	public $amount;
+};
 
-function bbp_unread_posts_Initialize(){
-	load_plugin_textdomain('bbpress-unread-posts-v2', false, 'bbpress-unread-posts-v2/languages/' );
-	wp_enqueue_style ( "bbpress_unread_posts_Style", plugins_url ( "style.css", __FILE__ ) );
-	if (is_user_logged_in ()) {
-		add_action ( "bbp_theme_before_topic_title", "bbp_unread_posts_PerformTopicSpecificActions" );
-		add_action ( "bbp_theme_before_topic_title", "bbp_unread_posts_IconWrapperBegin" );
-		add_action ( "bbp_theme_after_topic_meta", "bbp_unread_posts_IconWrapperEnd" );
-		add_action ( "bbp_template_after_single_topic", "bbp_unread_posts_OnTopicVisit" );
-		add_filter ( "bbp_get_breadcrumb", "bbp_unread_posts_MarkAllTopicsAsReadButtonFilter" );
-		
-		add_action ( 'bbp_theme_before_forum_title', 'bbp_unread_forum_icons' );
+add_action( 'init', 'bbp_unread_posts_initialize' );
+add_action( 'admin_menu', 'bbp_unread_posts_plugin_menu' );
+
+/**
+ * Function bbp_unread_posts_initialize.
+ */
+function bbp_unread_posts_initialize() {
+	load_plugin_textdomain( 'bbpress-unread-posts-v2', false, 'bbpress-unread-posts-v2/languages/' );
+	wp_enqueue_style( 'bbpress_unread_posts_Style', plugins_url( 'style.css', __FILE__ ) );
+	if ( is_user_logged_in() ) {
+		add_action( 'bbp_theme_before_topic_title', 'bbp_unread_posts_perform_topic_specific_actions' );
+		add_action( 'bbp_theme_before_topic_title', 'bbp_unread_posts_icon_wrapper_begin' );
+		add_action( 'bbp_theme_after_topic_meta', 'bbp_unread_posts_icon_wrapper_end' );
+		add_action( 'bbp_template_after_single_topic', 'bbp_unread_posts_on_topic_visit' );
+		add_filter( 'bbp_get_breadcrumb', 'bbp_unread_posts_mark_all_topics_as_read_button_filter' );
+		add_action( 'bbp_theme_before_forum_title', 'bbp_unread_forum_icons' );
 	}
 }
 
-function bbp_unread_posts_plugin_menu(){
-	add_options_page ( "Unread Posts", "Unread Posts", 'manage_options', 'bbpUnreadPosts', 'bpp_unread_settings_page' );
+/**
+ * Function bbp_unread_posts_plugin_menu.
+ */
+function bbp_unread_posts_plugin_menu() {
+	add_options_page( 'Unread Posts', 'Unread Posts', 'manage_options', 'bbpUnreadPosts', 'bpp_unread_settings_page' );
 }
 
-function bbp_unread_posts_PerformTopicSpecificActions(){
-	if (bbp_unread_posts_IsMarkAllTopicsAsReadRequested ()) {
-		$topicID = bbp_unread_posts_GetCurrentLoopedTopicID ();
-		bbp_unread_posts_UpdateLastTopicVisit ( $topicID );
+/**
+ * Function bbp_unread_posts_perform_topic_specific_actions.
+ */
+function bbp_unread_posts_perform_topic_specific_actions() {
+	if ( bbp_unread_posts_is_mark_all_topics_as_read_requested() ) {
+		$topic_id = bbp_unread_posts_get_current_looped_topic_id();
+		bbp_unread_posts_update_last_topic_visit( $topic_id );
 	}
 }
 
-function bbp_unread_posts_IconWrapperBegin(){
-	bbp_unread_posts_clog ( "topic: " . bbp_get_topic_title ( $topicID ) );
-	$topicID = bbp_unread_posts_GetCurrentLoopedTopicID ();
-	$isUnreadTopic = bbp_is_topic_unread ( $topicID );
-	bbp_unread_posts_clog ( "lastactive: " . $topicLastActiveTime . "   lastvisit: " . $lastVisitTime );
+/**
+ * Function bbp_unread_posts_icon_wrapper_begin.
+ */
+function bbp_unread_posts_icon_wrapper_begin() {
+	$topic_id = bbp_unread_posts_get_current_looped_topic_id();
+	bbp_unread_posts_clog( 'topic: ' . bbp_get_topic_title( $topic_id ) );
+	$is_unread_topic = bbp_is_topic_unread( $topic_id );
+	bbp_unread_posts_clog( 'lastactive: lastvisit: ' );
 
-	
-	$unread_path = get_option ( 'bbp_unread_post_image_path_unread' );
-	$read_path = get_option ( 'bbp_unread_post_image_path_read' );
-	$realpath = ($isUnreadTopic ? $unread_path : $read_path);
-	if (empty( $unread_path ) || empty( $read_path )) {
-		$realpath =plugins_url ( "images/" . ($isUnreadTopic ? "folder_new.png" : "folder.png"), __FILE__ );
+	$unread_path = get_option( 'bbp_unread_post_image_path_unread' );
+	$read_path   = get_option( 'bbp_unread_post_image_path_read' );
+	$realpath    = ( $is_unread_topic ? $unread_path : $read_path );
+	if ( empty( $unread_path ) || empty( $read_path ) ) {
+		$realpath = plugins_url( 'images/' . ( $is_unread_topic ? 'folder_new.png' : 'folder.png' ), __FILE__ );
 	}
 	echo '
 			<div class="bbpresss_unread_posts_icon">
-				<a href="' . bbp_get_topic_last_reply_url ( $topicID ) . '">
+				<a href="' . bbp_get_topic_last_reply_url( $topic_id ) . '">
 					<img src="' . $realpath . '"/>
 				</a>
 			</div>
@@ -65,47 +92,74 @@ function bbp_unread_posts_IconWrapperBegin(){
 		';
 }
 
-function bbp_unread_posts_GetCurrentLoopedTopicID(){
-	return bbpress ()->topic_query->post->ID;
+/**
+ * Function bbp_unread_posts_get_current_looped_topic_id.
+ *
+ * @return mixed
+ */
+function bbp_unread_posts_get_current_looped_topic_id() {
+	return bbpress()->topic_query->post->ID;
 }
 
-function bbp_unread_posts_IconWrapperEnd(){
+/**
+ * Function bbp_unread_posts_icon_wrapper_end.
+ */
+function bbp_unread_posts_icon_wrapper_end() {
 	echo '
 			</div>
 		';
 }
 
-function bbp_unread_posts_OnTopicVisit(){
-	$topicID = bbpress ()->reply_query->query ["post_parent"];
-	bbp_unread_posts_UpdateLastTopicVisit ( $topicID );
+/**
+ * Function bbp_unread_posts_on_topic_visit.
+ */
+function bbp_unread_posts_on_topic_visit() {
+	$topic_id = bbpress()->reply_query->query['post_parent'];
+	bbp_unread_posts_update_last_topic_visit( $topic_id );
 }
 
-function bbp_unread_posts_UpdateLastTopicVisit($topicID){
-	bbp_unread_posts_clog ( "last visited update: " . current_time ( 'timestamp' ) );
-	update_post_meta ( $topicID, bbp_unread_posts_getLastVisitMetaKey (), current_time ( 'timestamp' ) );
+/**
+ * Function bbp_unread_posts_update_last_topic_visit
+ *
+ * @param int $topic_id Topic id.
+ */
+function bbp_unread_posts_update_last_topic_visit($topic_id) {
+	bbp_unread_posts_clog( 'last visited update: ' . current_time( 'timestamp' ) );
+	update_post_meta( $topic_id, bbp_unread_posts_get_last_visit_meta_key(), current_time( 'timestamp' ) );
 }
 
-function bbp_unread_posts_getLastVisitMetaKey(){
-	return "bbpress_unread_posts_last_visit_" . bbpress ()->current_user->id;
+/**
+ * Function bbp_unread_posts_get_last_visit_meta_key
+ *
+ * @return string
+ */
+function bbp_unread_posts_get_last_visit_meta_key() {
+	return 'bbpress_unread_posts_last_visit_' . bbpress()->current_user->id;
 }
 
-function bbp_unread_posts_MarkAllTopicsAsReadButtonFilter($content){
-	$topicId = bbp_get_topic_id ();
-	if ($topicId == 0) {
-		$forumId = bbp_get_forum_id ();
+/**
+ * Function bbp_unread_posts_mark_all_topics_as_read_button_filter.
+ *
+ * @param string $content Content.
+ * @return string
+ */
+function bbp_unread_posts_mark_all_topics_as_read_button_filter( $content ) {
+	$topic_id = bbp_get_topic_id();
+	if ( 0 === $topic_id ) {
+		$forum_id = bbp_get_forum_id();
 		$html = '
 				<div class="bbpress_mark_all_read_wrapper">
 					<form action="" method="post" class="bbpress_mark_all_read">
 						<input type="hidden" name="bbp_unread_posts_markAllTopicAsRead" value="1"/>
-						<input type="hidden" name="bbp_unread_posts_markID" value="' . $forumId . '"/>
+						<input type="hidden" name="bbp_unread_posts_mark_id" value="' . $forum_id . '"/>
 				';
-		if (bbp_unread_posts_IsMarkAllTopicsAsReadRequested ()) {
+		if ( bbp_unread_posts_is_mark_all_topics_as_read_requested() ) {
 			$html .= '
-						<span class="markedUnread" style="font-weight:bold;"'. __( 'Все топики отмечены как прочитанные','unread' ) .'></span>
+						<span class="markedUnread" style="font-weight:bold;"' . __( 'Все топики отмечены как прочитанные', 'unread' ) . '></span>
 					';
 		} else {
 			$html .= '
-						<input type="submit" value="' . __( 'Отметить все топики как прочитанные','unread' ) . '"/>
+						<input type="submit" value="' . __( 'Отметить все топики как прочитанные', 'unread' ) . '"/>
 					';
 		}
 		$html .= '
@@ -117,133 +171,160 @@ function bbp_unread_posts_MarkAllTopicsAsReadButtonFilter($content){
 	return $content;
 }
 
-function bbp_unread_posts_IsForumPage(){
-	return isset ( bbpress ()->topic_query->posts );
+/**
+ * Function bbp_unread_posts_is_forum_page.
+ *
+ * @return bool
+ */
+function bbp_unread_posts_is_forum_page() {
+	return isset( bbpress()->topic_query->posts );
 }
 
-function bbp_unread_posts_IsMarkAllTopicsAsReadRequested(){
-	return isset ( $_POST ["bbp_unread_posts_markAllTopicAsRead"] );
+/**
+ * Function bbp_unread_posts_is_mark_all_topics_as_read_requested.
+ *
+ * @return bool
+ */
+function bbp_unread_posts_is_mark_all_topics_as_read_requested(){
+	return isset ( $_POST ['bbp_unread_posts_markAllTopicAsRead'] );
 }
 
-function bbp_unread_posts_getRootForumId(){
-	return isset ( $_POST ["bbp_unread_posts_markID"] );
+/**
+ * Fucntion bbp_unread_posts_get_root_forum_id
+ *
+ * @return bool
+ */
+function bbp_unread_posts_get_root_forum_id() {
+	return isset ( $_POST['bbp_unread_posts_mark_id'] );
 }
 
-function bbp_is_topic_unread($topicID){
-	$topicLastActiveTime = bbp_convert_date ( get_post_meta ( $topicID, '_bbp_last_active_time', true ) );
-	$lastVisitTime = get_post_meta ( $topicID, bbp_unread_posts_getLastVisitMetaKey (), true );
-	bbp_unread_posts_clog ( "Last Active Time " . $topicLastActiveTime . "   >   last visit time" . $lastVisitTime );
-	return $topicLastActiveTime > $lastVisitTime;
+/**
+ * Function bbp_is_topic_unread
+ *
+ * @param int $topic_id Topic id.
+ * @return bool
+ */
+function bbp_is_topic_unread( $topic_id ) {
+	$topic_last_active_time = bbp_convert_date( get_post_meta( $topic_id, '_bbp_last_active_time', true ) );
+	$last_visit_time        = get_post_meta( $topic_id, bbp_unread_posts_get_last_visit_meta_key(), true );
+	bbp_unread_posts_clog( 'Last Active Time ' . $topic_last_active_time . '   >   last visit time' . $last_visit_time );
+	return $topic_last_active_time > $last_visit_time;
 }
 
-function bbp_unread_forum_icons(){
-	if ('forum' == get_post_type ()) {
-		$unreadInfo = null;
-		if (bbp_unread_posts_IsMarkAllTopicsAsReadRequested ()) {
-			// $forumId = bbp_unread_posts_getRootForumId();
-			$forumId = bbp_get_forum_id ();
-			bbp_markAllUnread ( $forumId );
+/**
+ * Fucntion bbp_unread_forum_icons.
+ */
+function bbp_unread_forum_icons() {
+	if ( 'forum' === get_post_type() ) {
+		$unread_info = null;
+		if ( bbp_unread_posts_is_mark_all_topics_as_read_requested() ) {
+			$forum_id = bbp_get_forum_id();
+			bbp_mark_all_unread( $forum_id );
 			$unread = false;
 		} else {
-			$forumId = bbp_get_forum_id ();
-			$unreadInfo = bbp_isForumUnread ( $forumId );
-			$unread = $unreadInfo->unread;
+			$forum_id    = bbp_get_forum_id();
+			$unread_info = bbp_is_forum_unread( $forum_id );
+			$unread      = $unread_info->unread;
 		}
-		$unreadamount = get_option ( 'bbp_unread_post_amount', false );
-		$amountDiv = '';
-		if($unreadamount && unreadInfo != null && $unreadInfo->amount > 0){
-			bbp_unread_posts_clog($unreadInfo->amount);
-			$amountDiv = '<span class="bbpresss_unread_posts_amount">'. $unreadInfo->amount .'</span>';
-			bbp_unread_posts_clog($amountDiv);
+		$unread_amount = get_option( 'bbp_unread_post_amount', false );
+		$amount_div    = '';
+		if ( $unread_amount && ! empty( $unread_info ) && $unread_info->amount > 0 ) {
+			bbp_unread_posts_clog( $unread_info->amount );
+			$amount_div = '<span class="bbpresss_unread_posts_amount">' . $unread_info->amount . '</span>';
+			bbp_unread_posts_clog( $amount_div );
 		}
-		$unread_path = get_option ( 'bbp_unread_post_image_path_unread' );
-		$read_path = get_option ( 'bbp_unread_post_image_path_read' );
-		$realpath = ($unread ? $unread_path : $read_path);
-		if (empty( $unread_path ) || empty( $read_path )) {
-			$realpath =plugins_url ( "images/" . ($unread ? "folder_new.png" : "folder.png"), __FILE__ );
+		$unread_path = get_option( 'bbp_unread_post_image_path_unread' );
+		$read_path   = get_option( 'bbp_unread_post_image_path_read' );
+		$real_path   = ( $unread ? $unread_path : $read_path );
+		if ( empty( $unread_path ) || empty( $read_path ) ) {
+			$real_path = plugins_url( 'images/' . ( $unread ? 'folder_new.png' : 'folder.png' ), __FILE__ );
 		}
-			
+
 		echo '
 			<div class="bbpresss_unread_posts_icon">
-				<a href="' . bbp_get_topic_last_reply_url ( $topicID ) . '">
-					<img src="' . $realpath . '"/>
+				<a href="' . bbp_get_topic_last_reply_url() . '">
+					<img src="' . $real_path . '"/>
 				</a>
-				'. $amountDiv. '	
+				' . $amount_div . '	
 			</div>
 			<div style="display:table-cell;">
 		';
 	}
 }
 
-function bbp_isForumUnread($forumId){
-	bbp_unread_posts_clog ( "forum: " . bbp_get_forum_title ( $forumId ) );
-	if ($forumId != null && ! empty ( $forumId )) {
-		$unreadamount = get_option ( 'bbp_unread_post_amount', false );
-		$unr = new unreadInformation();
-		$unr->unread = false;
-		$unr->amount = 0;
-		
-		$childs = bbp_get_all_child_ids ( $forumId, bbp_get_topic_post_type () );
-		$max = count ( $childs );
+/**
+ * Function bbp_is_forum_unread.
+ *
+ * @param int $forum_id Forum id.
+ * @return UnreadInformation
+ */
+function bbp_is_forum_unread( $forum_id ) {
+	bbp_unread_posts_clog( 'forum: ' . bbp_get_forum_title( $forum_id ) );
+	$unr = null;
+	if ( ! empty( $forum_id ) ) {
+		$unreadamount = get_option( 'bbp_unread_post_amount', false );
+		$unr          = new UnreadInformation();
+		$unr->unread  = false;
+		$unr->amount  = 0;
+		$childs       = bbp_get_all_child_ids( $forum_id, bbp_get_topic_post_type() );
+		$max          = count( $childs );
 
-		for($i = 0; $i <= $max; $i ++) {
-			$topicID = $childs[$i];
-			bbp_unread_posts_clog ( " -- found subtopic: $topicID" );
-			if (! empty ( $topicID ) && bbp_is_topic_unread ( $topicID )) {
-				bbp_unread_posts_clog ( " -- unread topic!" );
+		for ( $i = 0; $i <= $max; $i++ ) {
+			$topic_id = $childs[ $i ];
+			bbp_unread_posts_clog( " -- found subtopic: $topic_id" );
+			if ( ! empty( $topic_id ) && bbp_is_topic_unread( $topic_id ) ) {
+				bbp_unread_posts_clog( ' -- unread topic!' );
 				$unr->unread = true;
 				$unr->amount = $unr->amount + 1;
-				if(! $unreadamount){
+				if ( ! $unreadamount ) {
 					return $unr;
 				}
 			}
 		}
-		$childs = bbp_get_all_child_ids ( $forumId, bbp_get_forum_post_type () );
-		$max = count ( $childs );
-		$subforumID;
-		for($i = 0; $i <= $max; $i ++) {
-			$subforumID = $childs [$i];
-			bbp_unread_posts_clog ( " -- found subforum:" . bbp_get_forum_title ( $subforumID ) );
-			$subforuminfo = bbp_isForumUnread( $subforumID );
-			if (! empty ( $subforumID ) && $subforuminfo->unread) {
-				bbp_unread_posts_clog ( " -- unread forum!" );
+		$childs = bbp_get_all_child_ids( $forum_id, bbp_get_forum_post_type() );
+		$max    = count( $childs );
+		for ( $i = 0; $i <= $max; $i ++ ) {
+			$subforum_id = $childs[ $i ];
+			bbp_unread_posts_clog( ' -- found subforum:' . bbp_get_forum_title( $subforum_id ) );
+			$sub_forum_info = bbp_is_forum_unread( $subforum_id );
+			if ( ! empty( $subforum_id ) && $sub_forum_info->unread ) {
+				bbp_unread_posts_clog( ' -- unread forum!' );
 				$unr->unread = true;
-				$unr->amount = $subforuminfo->amount + $unr->amount;
-				if(! $unreadamount){
+				$unr->amount = $sub_forum_info->amount + $unr->amount;
+				if ( ! $unreadamount ) {
 					return $unr;
 				}
-				
 			}
 		}
 	}
 	return $unr;
 }
 
-function bbp_markAllUnread($forumId){
-	bbp_unread_posts_clog ( "marking all topic for forum: " . bbp_get_forum_title ( $forumId ) . " as unread" );
-	bbp_unread_posts_clog ( "forum id: " . $forumId );
-	if ($forumId != null && ! empty ( $forumId )) {
-		$childs = bbp_get_all_child_ids ( $forumId, bbp_get_topic_post_type () );
-		$max = count ( $childs );
-		$topicID;
-		for($i = 0; $i <= $max; $i ++) {
-			$topicID = $childs [$i];
-			bbp_unread_posts_clog ( "found topic " . bbp_get_topic_title ( $topicID ) . "with id : " . $topicID );
-			if (! empty ( $topicID ) && bbp_is_topic_unread ( $topicID )) {
-				bbp_unread_posts_UpdateLastTopicVisit ( $topicID );
-				bbp_unread_posts_clog ( "marking it as unread" );
+/**
+ * Function bbp_mark_all_unread.
+ *
+ * @param int $forum_id Forum id.
+ */
+function bbp_mark_all_unread( $forum_id ) {
+	bbp_unread_posts_clog( 'marking all topic for forum: ' . bbp_get_forum_title( $forum_id ) . ' as unread' );
+	bbp_unread_posts_clog( 'forum id: ' . $forum_id );
+	if ( ! empty( $forum_id ) ) {
+		$children = bbp_get_all_child_ids( $forum_id, bbp_get_topic_post_type() );
+		$max      = count( $children );
+		for ( $i = 0; $i <= $max; $i ++ ) {
+			$topic_id = $children[ $i ];
+			bbp_unread_posts_clog( 'found topic ' . bbp_get_topic_title( $topic_id ) . 'with id : ' . $topic_id );
+			if ( ! empty( $topic_id ) && bbp_is_topic_unread( $topic_id ) ) {
+				bbp_unread_posts_update_last_topic_visit( $topic_id );
+				bbp_unread_posts_clog( 'marking it as unread' );
 			}
 		}
-		$childs = bbp_get_all_child_ids ( $forumId, bbp_get_forum_post_type () );
-		$max = count ( $childs );
-		$subforumID;
-		for($i = 0; $i <= $max; $i ++) {
-			$subforumID = $childs [$i];
-			bbp_unread_posts_clog ( " -- found subforum " . bbp_get_forum_title ( $subforumID ) . "with id : " . $subforumID );
-			// bbp_unread_posts_clog ( !empty( $subforumID));
-			bbp_markAllUnread ( $subforumID );
+		$children = bbp_get_all_child_ids( $forum_id, bbp_get_forum_post_type() );
+		$max      = count( $children );
+		for ( $i = 0; $i <= $max; $i++ ) {
+			$subforum_id = $children[ $i ];
+			bbp_unread_posts_clog( ' -- found subforum ' . bbp_get_forum_title( $subforum_id ) . 'with id : ' . $subforum_id );
+			bbp_mark_all_unread( $subforum_id );
 		}
 	}
 }
-
-?>
